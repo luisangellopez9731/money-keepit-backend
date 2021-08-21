2d4c2ccaf2ccaa3ecccc0e1b57d659c91741cf23
luisangellopez9731 <luisangellopezhernandez1997@hotmail.com>
2021-08-21 13:11:07 -0400

    flow

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import workSpaceController from "./workspaceController";
-import usersController from "./usersController";
-import authController from "./auth";
-import {protect, protectWithoutWorkspace} from 'middlewares/auth'
+import workSpace from "./workspace";
+import users from "./users";
+import auth from "./auth";
+import { protect, protectWithoutWorkspace } from "middlewares/auth";
 
 const router = Router();
 
-router.use("/workspaces", protectWithoutWorkspace, workSpaceController);
-router.use("/users", usersController);
-router.use("/auth", authController);
+router.use("/workspaces", protectWithoutWorkspace, workSpace);
+router.use("/users", users);
+router.use("/auth/", auth);
 
 export default router;
--- src/controllers/workspace.ts
+++ src/controllers/workspace.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import { WorkspaceService } from "../services/workspace";
 import { getTokenInfo } from "utils";
 import { JwtPayload } from "jsonwebtoken";
+import { protect, protectWithoutWorkspace } from "middlewares/auth";
 
 const router = Router();
 
-router.post("/", (req, res) => {
-  res.send(WorkspaceService.create(req.body));
+router.post("/", async (req, res) => {
+  const tokenInfo = await getTokenInfo(req.headers.authorization as string);
+  const { id: userId } = tokenInfo;
+  const { name, description } = req.body;
+  res.send(WorkspaceService.create({ userId, name, description }));
 });
 
 router.get("/", async (req, res, next) => {
