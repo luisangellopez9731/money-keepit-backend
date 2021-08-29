f5b618e036e5ce57c84d350fca2996b59d100f32
luisangellopez9731 <luisangellopezhernandez1997@hotmail.com>
2021-08-29 10:40:41 -0400

    controllers for category and transactions

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,14 +1,17 @@
-import { Router } from "express";
-import workSpace from "./workspace";
-import users from "./users";
 import auth from "./auth";
-import { protect, protectWithoutWorkspace } from "middlewares/auth";
+import users from "./users";
+import { Router } from "express";
+import {
+  AccountRepository,
+  CategoryRepository,
+  TransactionRepository,
+} from "data/typeorm/repositories";
+import workSpace from "./workspace";
 import { AutoCrud } from "core/auto-rest-crud";
-import { AccountRepository } from "data/typeorm/repositories";
+import { protect, protectWithoutWorkspace } from "middlewares/auth";
 
 function router() {
   const router = Router();
-
   router.use("/workspaces", protectWithoutWorkspace, workSpace);
   router.use("/users", users);
   router.use("/auth/", auth);
@@ -22,6 +25,20 @@ function router() {
     app: router,
     path: "/accounts",
     repository: AccountRepository(),
+    options: commonOptions,
+  });
+
+  new AutoCrud({
+    app: router,
+    path: "/categories",
+    repository: CategoryRepository(),
+    options: commonOptions,
+  });
+
+  new AutoCrud({
+    app: router,
+    path: "/transactions",
+    repository: TransactionRepository(),
     options: commonOptions,
   });
 
